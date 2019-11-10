c4c7c30838c121b150d62a1464bef7e97a5fb136
zukitaa <44138005+zukitaa@users.noreply.github.com>
2019-11-10 10:29:06 -0800

    Update TodoList.py

--- TodoList.py
+++ TodoList.py
@@ -36,7 +36,7 @@ class Todo:
     self.data = datetime.datetime.now()  # generating datetime for this moment(when it would be used)
 
   def __str__(self): # this method is used to return todo in a nice way 
-    return f'Data: {self.data.strftime("%d/%m/%Y %H:%M")} \nTodo: {self.text}'
+    return f'Data: {self.data.strftime("%d/%m/%Y %H:%M:%S")} \nTodo: {self.text}'
 
 
 # I used Manager class for managing processes(adding to datebase from class Todo or Todo from file, also for remove, update and show todoes)
@@ -88,21 +88,22 @@ def menu():
       line_data = []
 
       for line in r:
-        line = line.split()  # spliting words in line
+        line = line.split()  # creating list of all words in the file 
 
-        if line[0] != 'Todo:':  # if first wor in line is not todo, there should be 1), 2) and etc.
-          del line[0]  # deleting 1)
-          data_text = ' '.join(line)
-          line_data.append(data_text)    
+        if line[0] != 'Todo:':  # if the first word in the line is not todo, there should be 1), 2) or etc.
+          del line[0]  # deleting first word on line, which is: 1)
+          data_text = ' '.join(line)  # coverting this words to string again
+          line_data.append(data_text)
         else:
           todo_text = ' '.join(line)
           line_todo.append(todo_text)
     
-      d = dict(zip(line_data,line_todo))
-      for k,v in d.items():
-        todo = Todo_from_file(f'{k}\n{v}')
-        manager.add_from_file(todo)
-      
+      d = dict(zip(line_data,line_todo))  # creating dict in which key is data and value is todo
+      for k,v in d.items():  
+        todo = Todo_from_file(f'{k}\n{v}')  # passing new todo with data to class todo_from_file
+        manager.add_from_file(todo)  # calling Manager method to add todo in Database  
+    
+    # Menue
     while choice != 'q':
       print('\n\n>>>> To do list Menu <<<<')
       print('a) Add')
@@ -112,62 +113,87 @@ def menu():
       print('q) Quit')
 
       choice = input('Action:  ').lower()
-
+      
+      # Creating todo
       if choice == 'a':
         text = input('To do:  ')
         todo = Todo(text)
         manager.add(todo)
 
-
+      # Editing already existed todo
       elif choice == 'e':
-        manager.show_all()      
+        manager.show_all() # showing all todoes with their indexes
         entries = database.get_all()
 
-        if len(entries) > 0:
+        if len(entries) > 0:  # if entries is not empty
 
           try:
-            index = int(input('enter index number to edit:  '))
+            index = int(input('enter index number to edit:  '))  # this is input for index, which u need for editing
           except IndexError:
             print('Invalid input')
           else:
-            index = entries[index - 1]
+            index = entries[index - 1]  # index - 1 because for now it starts with 1 
             text = input('To do:  ')
             new_todo = Todo(text)
-            manager.update(index, new_todo)
+            manager.update(index, new_todo)  # calling Manager class to update Database
 
-
+      # Deleting todo
       elif choice == 'd':
-        manager.show_all()
+        manager.show_all()  #showing all todoes to have choice
         entries = database.get_all()
 
-        if len(entries) > 0:
+        if len(entries) > 0:  # if it's not empty
           try:
-            index = int(input('enter index number to edit:  '))
-            index = entries[index - 1]
+            index = int(input('enter index number to edit:  '))  # index
+            index = entries[index - 1]  # indexing should start from 0
           except IndexError:
             print('Invalid input')
           else:
-            manager.delete(index)
+            manager.delete(index)  # calling Manager method delete 
 
-
+      # Show all
       elif choice == 's':
-        manager.show_all()
-
+        manager.show_all()  # Manager method show_all
+        
+      # Quiting while loop
       elif choice == 'q':
         save = input('Wanna save to file? [y/n]:').lower()
-
+      
+      # Saving to file
         if save == 'y':
-          f = open('Todo.txt', 'w')
-          f.write(manager.to_file())
-          f.close()
+          f = open('Todo.txt', 'w')  # open file in write mode
+          f.write(manager.to_file())  #calling Manager method to_file to save all todoes to file
+          f.close()  # closing file
           
         else:
           pass
-
       else:
         pass
 
 
-menu()
+menu()  #lesss go
+
+
+# >>>>>>>>>>>> Note <<<<<<<<<<<<<
+# it won't work if you create todo file yourself, because simply just one space is important in this Todo.txt file 
+# for example file looks like this:
+#                                   1) Data: 10/11/2019 18:02:01 
+#                                   Todo: note 1
+#                                   2) Data: 10/11/2019 18:02:02 
+#                                   Todo: note 2
+#                                   3) Data: 10/11/2019 18:02:03 
+#                                   Todo: note 3
+#                                   4) Data: 10/11/2019 18:02:04 
+#                                   Todo: note 4
+#                                   5) Data: 10/11/2019 18:02:05 
+#                                   Todo: note 5
+
+
+#  >>>>>>>>>>>>>>>>>>>>> You need to create todoes with this programm instead <<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+
+
+
+
 
 
